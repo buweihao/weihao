31a79b34d2bdf6203d5b8a06f4ea45372612f4dd
buweihao_macbookAirM1 <1109465339@qq.com>
2026-07-10 18:16:58 +0800

    UI/CMS: Refactor navbar and remove category schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,4 @@
 import { productType } from "./productType";
-import { categoryType } from "./categoryType";
 import { siteSettingsType } from "./siteSettingsType";
 
-export const schemaTypes = [productType, categoryType, siteSettingsType];
+export const schemaTypes = [productType, siteSettingsType];
