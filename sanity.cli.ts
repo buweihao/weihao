c9a79f5d2b976db7b7373ea407a6bc950d774183
buweihao_macbookAirM1 <1109465339@qq.com>
2026-07-10 16:59:05 +0800

    Fix: Hardcode Sanity projectId to x2e2nl4u

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,6 @@
 import { defineCliConfig } from "sanity/cli";
 
-const projectId =
-    process.env.SANITY_STUDIO_PROJECT_ID ??
-    process.env.PUBLIC_SANITY_PROJECT_ID ??
-    "";
+const projectId = "x2e2nl4u";
 const dataset =
     process.env.SANITY_STUDIO_DATASET ??
     process.env.PUBLIC_SANITY_DATASET ??
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./sanity/schemaTypes";
 
-const projectId =
-    process.env.SANITY_STUDIO_PROJECT_ID ??
-    process.env.PUBLIC_SANITY_PROJECT_ID ??
-    "";
+const projectId = "x2e2nl4u";
 const dataset =
     process.env.SANITY_STUDIO_DATASET ??
     process.env.PUBLIC_SANITY_DATASET ??
